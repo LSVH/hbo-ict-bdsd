d1e260e0561b7b1616f82be849843fccbf244709
Luuk van Houdt <luuk@vhoudt.nl>
2020-03-05 13:20:30 +0100

    Refactored App to ease up creation of another Map Reduce App

---
+++ src/main/java/org/lsvh/hu/bdsd/MapReduceApp.java
@@ -0,0 +1,64 @@
+package org.lsvh.hu.bdsd;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.fs.Path;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+
+public abstract class MapReduceApp {
+    private static final Logger log = Logger.getLogger(MapReduceApp.class);
+
+    private static final String INPUT_PATH = "data/input";
+    private static final String OUTPUT_PATH = "data/output";
+    private static final Integer JOB_ITERATIONS = 10;
+    private static final String OPT_IN = "input";
+    private static final String OPT_OUT = "output";
+    private static final String OPT_ITS = "iterations";
+
+    protected static Path in;
+    protected static Path out;
+    protected static Integer its;
+
+    protected MapReduceApp() {}
+
+    protected static void setupLogging() {
+        org.apache.log4j.BasicConfigurator.configure();
+        Logger.getRootLogger().setLevel(Level.ERROR);
+    }
+
+    protected static void setupCLI(String[] args) {
+        CommandLineParser parser = new DefaultParser();
+        Options options = new Options();
+
+        options.addOption("i", OPT_IN, true, "what files should be parsed");
+        options.addOption("o", OPT_OUT, true, "where the parsed files should be stored");
+        options.addOption("I", OPT_ITS, true, "how many times the job(s) should run");
+
+        try {
+            CommandLine line = parser.parse(options, args);
+            in = new Path(line.hasOption(OPT_IN) ? line.getOptionValue(OPT_IN) : INPUT_PATH);
+            out = new Path(line.hasOption(OPT_OUT) ? line.getOptionValue(OPT_OUT) : OUTPUT_PATH);
+            its = line.hasOption(OPT_ITS) ? Integer.parseInt(line.getOptionValue(OPT_ITS)) : JOB_ITERATIONS;
+        } catch (Exception e) {
+            log.error(e);
+        }
+    }
+
+    protected static void setupIO() {
+        File outputDir = new File(out.toString());
+        if (outputDir.isDirectory()) {
+            try {
+                FileUtils.deleteDirectory(outputDir);
+            } catch (IOException e) {
+                log.error(e);
+            }
+        }
+    }
+}
--- src/main/java/org/lsvh/hu/bdsd/pagerank/App.java
+++ src/main/java/org/lsvh/hu/bdsd/pagerank/App.java
@@ -1,72 +1,17 @@
 package org.lsvh.hu.bdsd.pagerank;
 
-import org.apache.commons.cli.*;
-import org.apache.commons.io.FileUtils;
-import org.apache.hadoop.fs.Path;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+import org.lsvh.hu.bdsd.MapReduceApp;
 import org.lsvh.hu.bdsd.pagerank.jobs.ParseInputJob;
 
-import java.io.File;
-import java.io.IOException;
-
-public class App {
-    private static final String INPUT_PATH = "data/input";
-    private static final String OUTPUT_PATH = "data/output";
-    private static final Integer JOB_ITERATIONS = 10;
-    private static final String OPT_IN = "input";
-    private static final String OPT_OUT = "output";
-    private static final String OPT_ITS = "iterations";
-
-    private static Path in;
-    private static Path out;
-    private static Integer its;
-
+public class App extends MapReduceApp {
     public static void main(String[] args) {
-        org.apache.log4j.BasicConfigurator.configure();
-
-        CommandLineParser parser = new DefaultParser();
-        Options options = new Options();
-
-        options.addOption("i", OPT_IN, true, "what files should be parsed");
-        options.addOption("o", OPT_OUT, true, "where the parsed files should be stored");
-        options.addOption("I", OPT_ITS, true, "how many times the job(s) should run");
-
-        try {
-            CommandLine line = parser.parse(options, args);
-            in = new Path(line.hasOption(OPT_IN) ? line.getOptionValue(OPT_IN) : INPUT_PATH);
-            out = new Path(line.hasOption(OPT_OUT) ? line.getOptionValue(OPT_OUT) : OUTPUT_PATH);
-            its = line.hasOption(OPT_ITS) ? Integer.parseInt(line.getOptionValue(OPT_ITS)) : JOB_ITERATIONS;
-            setup();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void setup() {
         setupLogging();
+        setupCLI(args);
         setupIO();
         setupJobs();
     }
 
-    private static void setupLogging() {
-        org.apache.log4j.BasicConfigurator.configure();
-        Logger.getRootLogger().setLevel(Level.ERROR);
-    }
-
-    private static void setupIO() {
-        File outputDir = new File(out.toString());
-        if (outputDir.isDirectory()) {
-            try {
-                FileUtils.deleteDirectory(outputDir);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     private static void setupJobs() {
-        ParseInputJob parseInput = new ParseInputJob(in, out);
-        parseInput.run();
+        new ParseInputJob(in, out).run();
     }
 }
